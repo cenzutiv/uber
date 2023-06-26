bd4dc2f28b93511857990c31b3365ceb9ce5ca6b
cenzutiv <someuselessusername@gmail.com>
2023-06-25 23:15:58 -0700

    Update 5sim.py

--- 5sim.py
+++ 5sim.py
@@ -1,7 +1,7 @@
 from operator import truediv
 import requests, time
 
-token = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTg5MDAzNzMsImlhdCI6MTYyNzM2NDM3MywicmF5IjoiMzI1OGQ1ODdhNzBlNTg4NjY1Y2NjNmQwZWU2ZDFlMWUiLCJzdWIiOjY4ODk0MX0.mSe5JUm9D67n7CZrmKtupB9CrMhaNg5yvhP8oUC2GrrFkUYvtwh6Vfrou9hyFTu0ql1L5BdLUxH0IdqPAX6QBof68htdWo7j-BBEM_j57vlOlhEHKRTJM01gen4abdABCiErE3tWSj4tAQVSOt0SdvI2sb3ArwAVgt-zl0Q4s58zKL5-WQYxiJyYaAQmJrClA_Df9nxpik3Vw_ERbOQk76vd3Tzd1sHZ41cvvYwoVWRfqD1jJUx41ZbFFZwBs6zMnsr0hOJI1yzmoDxADlcutPHaSV2lhI8-fDRtKgOiG7yT6f_SrMuNFSRHMRBiJBGTlCMMHTZSd-Ms-lHZJqrjcA'
+token = 'token'
 country = 'usa'
 operator = 'virtual23'
 product = 'uber'
